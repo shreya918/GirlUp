981568be569df9c617d788071f11e721fb987016
Chirag050 <148074355+Chirag050@users.noreply.github.com>
2024-08-28 18:09:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
